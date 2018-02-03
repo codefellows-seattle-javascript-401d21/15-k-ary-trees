be79b9652996f0fc6ddb0c888749320e82b582a3
Kevin Miller <kevin@driftabout.com>
2018-02-03 10:33:32 -0800

    added function to process

--- lab-kevin/index.js
+++ lab-kevin/index.js
@@ -17,41 +17,66 @@ fs.readFileProm(asset)
 
 function parseHtml(data) {
 
-  let tempArr = []
+  let tempArr = [];
   let html = data.toString('utf8');
 
   let elements = html.replace(/\n/g,'').replace(/<([^>]+)>/g, '\n<$1>\n')
     .split('\n').filter(elm => elm.trim()).slice(1);
-let tempElm;
-let cleanParent
-  var parent = elements[0] //.substr(1);
-  console.log(parent);
-  elements.forEach(elm => {
-    //if the element does not n=match its closing element
+
+  //parentStack.push(elements[0]);
+
+//let tempElm;
+//let cleanParent
+ // var parent = elements[0] //.substr(1);
+ // console.log(parent);
+  // elements.forEach(elm => {
+  //   //if the element does not n=match its closing element
    
-    if (elm.indexOf('</') === -1 ){ 
-    if (elm !== parent){ 
-      debug(elm, parent );
-      //get element name
-      tempElm = elm;
-      tempElm.replace(/([<</>])/g, '');
-      //inset the item in the tree at the parents value
-      //treeInsert.insert(elm. parentStack.head.value);
-      tempArr.push([parent, tempElm]);
-      //push
-      cleanParent = parent;
-      parentStack.push(cleanParent.replace(/([<</>])/g, ''));
-      debug('parent', parent);
-      debug('parentStack', parentStack);
-      if (elm.indexOf('<') !== -1) parent = elm;
-      debug('parent', parent);
-    }
-    } else {
-      debug('element is closing', elm );
-      parentStack.pop();
-      parent = parentStack.top.value;
-    }
+  //   if (elm.indexOf('</') === -1 ){ 
+  //   if (elm !== parent){ 
+  //     debug(elm, parent );
+  //     //get element name
+  //     tempElm = elm;
+  //     tempElm.replace(/([<</>])/g, '');
+  //     //inset the item in the tree at the parents value
+  //     //treeInsert.insert(elm. parentStack.head.value);
+  //     tempArr.push([parent, tempElm]);
+  //     //push
+  //     cleanParent = parent;
+  //     parentStack.push(cleanParent.replace(/([<</>])/g, ''));
+  //     debug('parent', parent);
+  //     debug('parentStack', parentStack);
+  //     if (elm.indexOf('<') !== -1) parent = elm;
+  //     debug('parent', parent);
+  //   }
+  //   } else {
+  //     debug('element is closing', elm );
+  //     parentStack.pop();
+  //     parent = parentStack.top.value;
+  //   }
+  // });
+
+
+  elements.forEach(elm => {
+    processElement(elm);
   });
+
+  function processElement(data){
+    //closing tag 
+    if ( /^<\/[^<]+>$/.test(data) ) return parentStack.pop();
+     
+    let parent = parentStack.top ? parentStack.top.value : data;
+    parentStack.push(data.replace(/([<</>])/g, ''));
+    
+    //opening
+    if (/^<[^<]+>$/.test(data)) return tempArr.push([parent, data]);
+    
+    //text
+    parentStack.pop();
+    //add as tex data
+    tempArr.push([parent, data]);
+    return;
+  }
 
   console.log('tempArr', tempArr );
 
