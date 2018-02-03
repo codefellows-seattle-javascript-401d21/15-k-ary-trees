df01a391dbd1f491164c434cdce024d3b750c4c9
Roger <roger.neil.davenport@gmail.com>
2018-02-03 13:52:56 -0800

    body parser working with readfile

--- lab-roger/index.js
+++ lab-roger/index.js
@@ -5,32 +5,36 @@ const parser = require('./lib/parser.js');
 const ktree = require('./lib/kary-tree');
 const fs = require ('fs');
 
-fs.readFile('./assets/minimal.html', (err, data) => {
+var elArray;
+
+elArray = fs.readFileSync('./assets/minimal.html', (err, data) => {
+
+
   if (err) throw err;
+  console.log(elArray);
 
-  let test = parser(data);
-  // let tempData = data.toString().split(/\n/gi).map(x => x.trim());
-  // let x =tempData.join('');
-  // let splitData = x.match(/<\/?\w+>/gi);
-
-  // console.log(splitData, splitData.length);
-  // let stack = new Stack();
-  // let tree = new ktree;
-  // tree.insert(splitData[0], splitData[0]);
-  // if (!stack.top) { stack.push(splitData[0]); }
-  // console.log('split data 2', splitData[2]);
-  // console.log('cat cat string', `/${stack.top.value}`)
-
-  // for (let i = 0; i < splitData.length; i ++) {
-  //   let parent = stack.top;
-  //   console.log('stack top', stack.top.value);
-  //   let check = `/${stack.top.value}`;
-  //   if (splitData[i] === check) {
-  //     console.log("_____", splitData[i]);
-  //     tree.insert(splitData[i],  parent);
-  //   }
-  //   stack.push(splitData[i]);
-    
-  // }
-  // console.dir(tree, {depth: null});
+  
+  
 });
+let newarray = parser(elArray);
+console.log('outside', newarray);
+let stack = new Stack();
+let tree = new ktree;
+
+
+tree.insert(newarray[0], newarray[0]);
+if (!stack.top) { stack.push(newarray[0]); }
+
+
+for (let i = 0; i < newarray.length; i ++) {
+  let parent = stack.top;
+ 
+ 
+  if (newarray[i] === stack.top.value) {
+    console.log('_____', newarray[i]);
+    tree.insert(newarray[i],  parent);
+  }
+  stack.push(newarray[i]);
+    
+}
+console.dir(tree, {depth: null});
