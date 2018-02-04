c8b224d9669c65d5e299ffb4ba0e6b2b6d7b9ca8
Kevin Miller <kevin@driftabout.com>
2018-02-03 16:51:43 -0800

    logic working

--- lab-kevin/index.js
+++ lab-kevin/index.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 const Promise = require('bluebird');
@@ -12,65 +11,30 @@ const asset =`${__dirname}/../assets/minimal.html`;
 const parentStack = new Stack();
 const htmlTree = new Tree();
 
-fs.readFileProm(asset)
-  .then(parseHtml)
-  .catch(console.error);
+fs.readFileProm(asset, 'utf8').then(parseHtml).catch(console.error);
 
-function parseHtml(fileData) {
-
-  let tempArr = [];
-  let html = fileData.toString('utf8');
-
-  let elements = html.replace(/\n/g,'').replace(/<([^>]+)>/g, '\n<$1>\n')
-    .split('\n').filter(elm => elm.trim()).slice(1);
+function parseHtml(html) {  
+  let elements = html.replace(/\n/g,'').replace(/</g, '\n<').split('\n').map(elm => elm.trim()).filter(val => val).slice(1);
 
   elements.forEach(elm => {
     processElement(elm);
   });
+ 
 
   function processElement(data){
-
-    if ( /^<\/[^<]+>$/.test(data) ){    //closing tag 
-      console.log('closing', data);
-      let val = parentStack.pop();
-      let parent = parentStack.top ? parentStack.top.value : '';
-      //tempArr.push([parent, val]);
-      htmlTree.insertData(val, parent);
-      return; 
-
-    }
-
-
-    if (/^<[^<]+>$/.test(data)){ //opening tag
-      console.log('opening', data);
-      let data_set = {eleName: data.replace(/([<</>])/g, ''), textContent: null};
-      //htmlTree.insertData(data_set, parent)
-      //tempArr.push([parent, data_set]);
-      parentStack.push(data_set);
-      return;
-    }
+    if ( /^<\/[^<]+>$/.test(data) ) return parentStack.pop(); //closing tag 
     
-    //return htmlTree.insert(data_set, parent); //tempArr.push([parent, data]); //opening
-    ///parentStack.pop();  //text
-    parentStack.top.value.textContent = data;
-    console.log('text', parentStack.top.value);
-    //tempArr.push([parent, data]);  //add as tex data
-    return;
-  }
+    let data_set = {eleName: data.replace(/([<</>])/g, ''), textContent: null};
+    let parent = parentStack.top ? parentStack.top.value : '';
+    parentStack.push(data_set);
   
-  // function processElement(data){
-  //   if ( /^<\/[^<]+>$/.test(data) ) return parentStack.pop(); //closing tag 
+    if (! /^<[^<]+>$/.test(data)) { //open tag with text
+      let dataArray = data.split('>');
+      data_set = {eleName: dataArray[0].replace(/([<</>])/g, ''), textContent: dataArray[1]};
+    }
 
-  //   let parent = parentStack.top ? parentStack.top.value : '';
-  //   parentStack.push(data.replace(/([<</>])/g, ''));
-  //   let data_set = {eleName: data, textContent: null};
-    
-  //   if (/^<[^<]+>$/.test(data)) return tempArr.push([parent, data]); //htmlTree.insert(data, parent); //tempArr.push([parent, data]); //opening
-  //   parentStack.pop();  //text
-  //   tempArr.push([parent, data]);  //add as tex data
-  //   return;
-  // }
+    htmlTree.insertData(data_set, parent);
+  }
 
-  console.log('tempArr', tempArr );
-
+  console.log('htmlTree',  JSON.stringify(htmlTree));
 } 
