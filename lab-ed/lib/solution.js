031ed8603999154c3d2ee1b9a0baaafc66b9bb5f
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-02-04 18:18:17 -0800

    DRY

--- lab-ed/lib/solution.js
+++ lab-ed/lib/solution.js
@@ -14,69 +14,36 @@ solution.htmlTree = function(tree) {
       // get rid of all html that starts with </
       arr = html.split('\n').filter(ele => ele.match(/<\w/))
       console.log(arr)
-      
       for (let i = 0; i < arr.length; i++) {
         //get the root element
         if (arr[i].charAt(0) === '<' ) tree.insert(arr[i])
         //get element at 2 indent
         if (arr[i].charAt(2) === '<' ) tree.insert(arr[i], arr[0])
         //get element at 4 indent
-        if (arr[i].charAt(4) === '<' ) {
-          var four = ''
-          var fourText = ''
-          for (var j = 0; arr[i][j] !== '>'; j++) {
-            four += arr[i][j]
-          }
-          four += arr[i][j]
-          //get text if any
-          j++
-          getText(j, i, arr)
-          
-          tree.insert(four, arr[2])
-          console.log(four)
-        }
+        if (arr[i].charAt(4) === '<' ) getElement(i, arr, tree)
         //get element at 6 indent
-        if (arr[i].charAt(6) === '<' ) {
-          var six = ''
-          for (var l = 0; arr[i][l] !== '>'; l++) {
-            six += arr[i][l]
-          }
-          six += arr[i][l]
-          l++
-          getText(l, i, arr)
-          tree.insert(six, four)
-          console.log(six)
-        }
+        if (arr[i].charAt(6) === '<' ) getElement(i, arr, tree)
         //get element at 8 indent
-        if (arr[i].charAt(8) === '<' ) {
-          var eight = ''
-          for (var m = 0; arr[i][m] !== '>'; m++) {
-            eight += arr[i][m]
-          }
-          eight += arr[i][m]
-          m++
-          getText(m, i, arr)
-          tree.insert(eight, four)
-          console.log(eight)
-        }
+        if (arr[i].charAt(8) === '<' ) getElement(i, arr, tree)
         //get element at 10 indent
-        if (arr[i].charAt(10) === '<' ) {
-          var ten = ''
-          for (var n = 0; arr[i][n] !== '>'; n++) {
-            ten += arr[i][n]
-          }
-          ten += arr[i][n]
-          n++
-          getText(n, i, arr)
-          tree.insert(ten, four)
-          console.log(ten)
-        }
+        if (arr[i].charAt(10) === '<' ) getElement(i, arr, tree)
       }
       console.log(tree)
     })
   return tree
 }
 
+function getElement(arrindex, arr, tree) {
+  var element = ''
+  for (var x = 0; arr[arrindex][x] !== '>'; x++) {
+    element += arr[arrindex][x]
+  }
+  element += arr[arrindex][x]
+  x++
+  getText(x, arrindex, arr)
+  tree.insert(element, arr[x])
+  console.log(element)
+}
 
 function getText(eleIndex, arrIndex, arr) {
   if (eleIndex < arr[arrIndex].length) {
